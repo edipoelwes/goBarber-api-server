9525b74213cdc2b6b63bc67ceaf997f66c4f356c
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-08-14 14:05:44 -0300

    alterando rota post para get

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,15 +2,8 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.post("/users", (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  response.json(user);
-});
+routes.get("/", (request, response) =>
+  response.json({ message: "Hello World!" }),
+);
 
 export default routes;
