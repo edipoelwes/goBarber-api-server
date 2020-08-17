5c5cd2e4090848bf24ca1bf32732eb8f395948ab
Edipo Elwes <edipoelwes2@ufpi.edu.br>
2020-08-16 23:19:34 -0300

    create table appointments and default: uuid_generate_v4()

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express, { json } from "express";
+import "reflect-metadata";
+import express from "express";
+import bodyParser from "body-parser";
 import routes from "./routes";
 
 import "./database";
 
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
 app.use(routes);
-app.use(json());
 
 app.listen(3333, () => {
   console.log("Server is running on port http://localhost:3333");
